db97fd1b39b420716ff7d0c69c7fe72266cdae7e
lokeshswamy <lokeswaraswamy.gubba@ideabytes.com>
2017-10-12 00:31:00 +0530

    VB 0.3.4

--- ng2-admin/src/app/pages/modules/components/contacts/data-filter.pipe.ts
+++ ng2-admin/src/app/pages/modules/components/contacts/data-filter.pipe.ts
@@ -8,7 +8,9 @@ export class DataFilterPipe implements PipeTransform {
 
     transform(array: any[], query: string): any {
         if (query) {
-            return _.filter(array, row=>row.name.indexOf(query) > -1);
+            return _.filter(array, row=> (row.name).toLowerCase().indexOf(query.toLowerCase()) > -1 
+            || row.email.toLowerCase().indexOf(query.toLowerCase()) > -1 
+            || row.company.toLowerCase().indexOf(query.toLowerCase()) > -1);
         }
         return array;
     }
